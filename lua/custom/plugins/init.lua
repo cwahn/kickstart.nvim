34455da85ebe1416b9c09912d994dd4e3532a17e
cwahn <cwahn0904@gmail.com>
2026-04-08 20:31:49 +0900

    Update custom plugins configuration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,4 +14,22 @@ return {
   {
     'mfussenegger/nvim-dap',
   },
+  {
+    'github/copilot.vim',
+    event = 'InsertEnter',
+    cmd = 'Copilot',
+    init = function()
+      -- Disable default <Tab> map to avoid conflict with blink.cmp
+      vim.g.copilot_no_tab_map = true
+    end,
+    config = function()
+      -- <C-J>          accept full suggestion  (official docs example)
+      -- <C-L>          accept next word        (insert mode only; no conflict with <C-l> window nav which is normal mode)
+      -- <M-]> / <M-[>  next / previous         (copilot.vim defaults, require option_as_alt in alacritty)
+      -- <C-]>          dismiss                 (copilot.vim default)
+      -- <M-\>          explicit trigger        (copilot.vim default)
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("")', { expr = true, replace_keycodes = false, desc = 'Copilot: accept suggestion' })
+      vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)', { desc = 'Copilot: accept next word' })
+    end,
+  },
 }
